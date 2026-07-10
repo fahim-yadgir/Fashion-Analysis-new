480757d0f18a8638699dc6cc04d585dddb65370b
Fahim <fahimyadgir07@gmail.com>
2026-07-10 14:38:38 +0530

    perform one case statement for customer buying segment

--- Fashion Analysis.sql
+++ Fashion Analysis.sql
@@ -14,3 +14,14 @@ update customer_purchase_dataset
 set `Customer Rating` = 3
 where `Customer Rating` = 3.22;
 
+select *,
+case 
+when `Unit Selling Price` < 400 then 'low'
+when `Unit Selling Price` between 400 and 500 then 'medium'
+when `Unit Selling Price` > 500 then 'high'
+end segment
+from customer_purchase_dataset;
+
+select `Product Name` ,brand,`Shopping App`,`Unit Selling Price`,`Payment Method`
+from customer_purchase_dataset
+where `Unit Selling Price` = (select max(`Unit Selling Price`)from customer_purchase_dataset);
